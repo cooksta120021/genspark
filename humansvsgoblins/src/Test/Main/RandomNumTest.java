28c148f9a64c0a6cae30361f8eea20a44bf897d0
cooksta120021 <cooksta120021@gmail.com>
2021-12-13 10:36:37 -0600

    had to remake the project due to framework support.

---
+++ humansvsgoblins/src/Test/Main/RandomNumTest.java
@@ -0,0 +1,22 @@
+package Main;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomNumTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void generator() {
+    }
+}
